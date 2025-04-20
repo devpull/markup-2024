29688c81636c1cf5d5cf832adaff97101a86e07a
devpull <devpull@yandex.ru>
2025-04-20 15:27:46 +0300

    Mobile changes

--- src/js/modules/hamburger.js
+++ src/js/modules/hamburger.js
@@ -1,20 +1,20 @@
-(function() {
-
+(function () {
   const menuMobile = document.getElementById("main-nav-mobile");
   const hamburger = document.getElementById("hamburger");
   const menuMobileClose = document.getElementById("main-nav-mobile-close");
+  const classNavOpen = 'h-nav-mobile-open';
 
-  if(!hamburger || !menuMobile || !menuMobileClose) {
+  if (!hamburger || !menuMobile || !menuMobileClose) {
     return;
   }
 
-  hamburger.addEventListener('click', (e) => {
-    menuMobile.style.display = 'block';
+  hamburger.addEventListener("click", (e) => {
+    menuMobile.style.display = "block";
+    document.body.classList.add(classNavOpen);
   });
 
-  menuMobileClose.addEventListener('click', (e) => {
-    menuMobile.style.display = 'none';
+  menuMobileClose.addEventListener("click", (e) => {
+    menuMobile.style.display = "none";
+    document.body.classList.remove(classNavOpen);
   });
-
-
 })();
